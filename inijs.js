492bcdd8057aa44bbf4a61b9c21223e800027ea6
genthadjamal <68898618+genthadjamal@users.noreply.github.com>
2020-12-30 09:58:20 +0700

    Create inijs.js

---
+++ inijs.js
@@ -0,0 +1,40 @@
+const text = document.querySelector(".gentha");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for(let i=0; i < splitText.length; i++){
+	text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+	const span = text.querySelectorAll('span')[char];
+	span.classList.add('fade');
+	char++
+	if(char == splitText.length){
+		complete();
+		return;
+	}
+}
+
+function complete(){
+	clearInterval(timer);
+	timer = null;
+}
+
+window.onscroll=function(){
+	const docScrollTop=document.documentElement.scrollTop;
+
+	if(window.innerWidth>991){
+		if(docScrollTop>100){
+			document.querySelector("header").classList.add("fixed")
+		}
+		else{
+			document.querySelector("header").classList.remove("fixed")
+		}
+	}
+
+}
